117d5ac57fdeb5f0a386ea397c2eda38b4e54d41
JustinasPakalnis <justinas.p@inbox.lt>
2024-07-24 10:58:20 +0300

    s

---
+++ kintamieji/number.js
@@ -0,0 +1,85 @@
+console.log (123);
+console.clear ();
+const age = 99;
+console.log (age);
+
+const mark1 = 10;
+const mark2 = 2;
+const mark3 = 8;
+const mark4 = 4;
+const mark5 = 6;
+
+const marksTotal = mark1 + mark2 + mark3 + mark4 + mark5;
+console.log (marksTotal);
+
+const marksAverage = (marksTotal / 5);
+console.log (marksAverage);
+
+let marksSum = 0;
+console.log (marksSum);
+
+marksSum = 99999;
+console.log (marksSum);
+console.clear ();
+
+let total = 0;
+console.log (total);
+
+total = total+100;
+console.log (total);
+
+total = total/5;
+console.log (total);
+
+total = total-2;
+console.log (total);
+
+total = total*3;
+console.log (total);
+
+total = total%5;
+console.log (total);
+
+
+const number = 54;
+const div = 5;
+
+const mod = number % div;
+const integer = (number - mod) / div;
+const integer2 = (number - mod % div) / div;
+
+
+console.log (mod);
+console.log (integer);
+console.log (integer2);
+
+console.clear ();
+
+const day = 10;
+const weekday = day % 7;
+
+console.log (weekday);
+
+/*
+1   1   8   15
+2   2   9   16
+3   3   10  17
+4   4   11  18
+5   5   12  19
+6   6   13  20
+0   7   14  21
+*/
+
+console.clear ();
+
+let gg = 0;
+console.log (gg);
+
+gg = gg + 100;
+console.log (gg);
+
+gg += + 100;
+console.log (gg);
+
+gg += + 100;
+console.log (gg);
