ac336defad04944a3425721aa084721636de3aef
JustinasPakalnis <justinas.p@inbox.lt>
2024-08-01 13:59:32 +0300

    asd

---
+++ function/import.js
@@ -0,0 +1,68 @@
+import { sum } from '../lib/sum.js'
+import { diff } from '../lib/diff.js'
+import { multi } from '../lib/multi.js'
+import { div } from '../lib/div.js'
+import { plus, minus, star, slash} from '../lib/math.js'
+
+
+
+const a = sum(7, 5)
+console.log(a);
+
+const b = diff(7, 5)
+console.log(b);
+
+const c = multi(7, 5)
+console.log(c);
+
+const d = div(7, 5)
+console.log(d);
+
+const e = plus(7, 5)
+console.log(e);
+
+const f = minus(7, 5)
+console.log(f);
+
+const g = star(7, 5)
+console.log(g);
+
+const h = slash(7, 5)
+console.log(h);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
