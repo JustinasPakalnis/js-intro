eb1fa7331a6113a1a556975d60b4dedfbfcf83fd
JustinasPakalnis <justinas.p@inbox.lt>
2024-08-23 10:40:00 +0300

    mokomes

--- codewars.js
+++ codewars.js
@@ -1430,3 +1430,164 @@ console.log("anas"[0].toUpperCase());
 console.log("anas"[0]);
 const str = "Namas";
 console.log(str[0] === str[0].toUpperCase());
+
+console.clear();
+console.log("-----------------------------------------------------");
+
+/*
+1. Skaičiaus lygumas
+Parašyk funkciją, kuri priima skaičių ir grąžina "Even", jei skaičius yra 
+lyginis, ir "Odd", jei skaičius yra nelyginis.
+ */
+
+function lyginis(skaicius) {
+  // if (skaicius % 2 === 0) {
+  //   return "lyginis";
+  // } else {
+  //   return "nelyginis";
+  // }
+  return skaicius % 2 === 0 ? "lyginis" : "nelyginis";
+}
+console.log(lyginis(40));
+console.log("-----------------------------------------------------");
+/*
+2. Pasukant tekstą atvirkščiai
+Parašyk funkciją, kuri priima eilutę (tekstą) ir grąžina tą eilutę atvirkštine tvarka.
+ */
+function reverse(tekstas) {
+  return tekstas.split("").reverse().join("");
+}
+console.log(reverse("Laba diena BIT"));
+// .reverse().join("");
+console.log("-----------------------------------------------------");
+/*
+3. Skaičių kvadratai
+Parašyk funkciją, kuri priima masyvą skaičių ir grąžina naują masyvą, kurio kiekvienas elementas yra
+atitinkamo pradinio masyvo elemento kvadratas.
+*/
+
+function kvadratu(array) {
+  return array.map((a) => a * a);
+}
+console.log(kvadratu([10, 2, 6, 4, 8, 8, 10, 16]));
+console.log("-----------------------------------------------------");
+/*
+4. Skaičių suma
+Parašyk funkciją, kuri priima du skaičius ir grąžina jų sumą.
+*/
+function suma(a, b) {
+  return a + b;
+}
+console.log(suma(2, 3));
+
+console.log("-----------------------------------------------------");
+/*
+5. Kelių skaičių suma
+Parašyk funkciją, kuri priima masyvą skaičių ir grąžina jų sumą.
+*/
+
+function reduce(skaiciai) {
+  return skaiciai.reduce((a, b) => a + b);
+}
+console.log(reduce([10, 2, 6, 4, 8, 8, 10, 16]));
+console.log("-----------------------------------------------------");
+/*
+6. Teksto simbolių skaičiavimas
+Parašyk funkciją, kuri priima eilutę ir grąžina objektą, kuriame yra skirtingų simbolių skaičius.
+*/
+// function diff(text) {
+//   const array = text.split("");
+//   console.log(array);
+// let index = 0;
+//   for (const count of array) {
+//     for ()
+//   }
+// }
+function diff(text) {
+  const characterCount = {};
+  for (let char of text) {
+    if (characterCount[char]) {
+      characterCount[char]++;
+    } else {
+      characterCount[char] = 1;
+    }
+  }
+  return characterCount;
+}
+console.log(diff("Laba diena su vistiena"));
+
+console.log("-----------------------------------------------------");
+/*
+7. Didžiausias skaičius
+Parašyk funkciją, kuri priima masyvą skaičių ir grąžina didžiausią skaičių iš masyvo.
+*/
+
+function didziausias(skaiciai) {
+  return skaiciai.reduce((a, b) => (a > b ? a : b));
+}
+console.log(didziausias([10, 2, 6, 4, 8, 8, 10, 16]));
+console.log("-----------------------------------------------------");
+/*
+8. Mažiausias skaičius
+Parašyk funkciją, kuri priima masyvą skaičių ir grąžina mažiausią skaičių iš masyvo.
+*/
+function maziausias(skaiciai) {
+  return skaiciai.reduce((a, b) => (a < b ? a : b));
+}
+console.log(maziausias([10, 2, 6, 4, 8, 8, 10, 16]));
+console.log("-----------------------------------------------------");
+/*
+9. Daugkartiniai žodžiai
+Parašyk funkciją, kuri priima žodį ir skaičių, ir grąžina tą žodį, kartotą tiek kartų, kiek nurodytas skaičius.
+*/
+function pakartotasZodis(zodis, skaicius) {
+  return zodis.repeat(skaicius);
+}
+console.log(pakartotasZodis("Mokomes ", 5));
+console.log("-----------------------------------------------------");
+/*
+10. Fibonacci seka
+Parašyk funkciją, kuri grąžina pirmuosius n Fibonacci sekos skaičius.
+*/
+function fibonacciGenerator(n) {
+  let output = [];
+  if (n === 1) {
+    output = [0];
+  } else if (n === 2) {
+    output = [0, 1];
+  } else {
+    output = [0, 1];
+    for (let i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+  }
+  return output;
+}
+console.log(fibonacciGenerator(10));
+console.log("-----------------------------------------------------");
+/*
+11. Masyvo elementų skaičiavimas
+Parašyk funkciją, kuri priima masyvą ir grąžina jo ilgį (elementų skaičių).
+*/
+function arrayLength(array) {
+  return array.length;
+}
+console.log(arrayLength([10, 2, 6, 4, "Pavadinimas", "zodis"]));
+console.log("-----------------------------------------------------");
+/*
+12. Filtravimas pagal sąlygas
+Parašyk funkciją, kuri priima masyvą skaičių ir grąžina naują masyvą, kuriame yra tik teigiami skaičiai.
+*/
+function teigiami(skaiciai) {
+  return skaiciai.filter((skc) => skc > 0);
+}
+console.log(teigiami([10, -2, 6, -4, 8, 8, -10, 16]));
+console.log("-----------------------------------------------------");
+/*
+13. Rask eilutės ilgį
+Parašyk funkciją, kuri priima eilutę ir grąžina tos eilutės ilgį.
+*/
+function ilgis(eilute) {
+  return eilute.length;
+}
+console.log(ilgis("labadiena su vistiena"));
--- methods/array-map.js
+++ methods/array-map.js
@@ -4,66 +4,63 @@
 
  */
 
-
 console.log(Array.prototype.map); // ciklas eina per masyva isitruakia kiekvinea reiksme ir ka nors atlieka
 
-const a = [10, 2, 8, 4, 6]
+const a = [10, 2, 8, 4, 6];
 console.log(a);
 
-
-const a2 = a.map(n => n*2);
+const a2 = a.map((n) => n * 2);
 console.log(a2);
 
-
-const a3 = a.map(n => n + 3);
+const a3 = a.map((n) => n + 3);
 console.log(a3);
 
 const a32 = a.map((n) => n + 3);
 console.log(a32);
 
-const a33 = a.map((n) =>  { return n + 3 } );
+const a33 = a.map((n) => {
+  return n + 3;
+});
 console.log(a33);
 
-const a34 = a.map(function (n) { return n + 3 } );
+const a34 = a.map(function (n) {
+  return n + 3;
+});
 console.log(a34);
 
-
-
-function plusThree (n) {
-    return n + 3 } ; 
+function plusThree(n) {
+  return n + 3;
+}
 const a35 = a.map(plusThree);
 console.log(a35);
 
-
 const names = [
-    'Jonas Jonaitis', 
-    'Marytė Marytaitė', 
-    'Petras Petraitis', 
-    'Ona Onaitė']
-const abbr2 =[];
+  "Jonas Jonaitis",
+  "Marytė Marytaitė",
+  "Petras Petraitis",
+  "Ona Onaitė",
+];
+const abbr2 = [];
 
 for (const name of names) {
-
-    abbr2.push(
+  abbr2.push(
     name
-        .split(' ')
-        .map(word => word[0]+'.')
-        .join(''));
+      .split(" ")
+      .map((word) => word[0] + ".")
+      .join("")
+  );
 }
 
 console.log(abbr2);
 
-
-const abbr3 =names.map(
-    name => name
-    .split(' ')
-    .map(word => word[0]+'.')
-    .join(''));
-
-
+const abbr3 = names.map((name) =>
+  name
+    .split(" ")
+    .map((word) => word[0] + ".")
+    .join("")
+);
 
 console.log(abbr3);
-
 
 /* 
 Kaip is pilno vardo gaunami inicialai
@@ -84,17 +81,3 @@ Jonas Jonaitis
 join ('')
 'J. J.'
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
